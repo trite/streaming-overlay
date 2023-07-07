9acdc024afbd7a4b12ec020859447ebaca3e5376
Paul Brunner <trite@users.noreply.github.com>
2023-07-07 16:51:28 -0700

    changes made by create-relay-app to enable relay
    https://github.com/tobias-tengler/create-relay-app

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { RelayEnvironmentProvider } from "react-relay";
+import { RelayEnvironment } from "./RelayEnvironment";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  gql,
+} from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'http://localhost:5433/graphql',
+  uri: "http://localhost:5433/graphql",
   cache: new InMemoryCache(),
 });
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <ApolloProvider client={client}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ApolloProvider>,
-)
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <RelayEnvironmentProvider environment={RelayEnvironment}>
+    <ApolloProvider client={client}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ApolloProvider>
+  </RelayEnvironmentProvider>
+);
