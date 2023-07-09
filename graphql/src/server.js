66d788d939566ed163e658cf48d483c1256457bc
Paul Brunner <trite@users.noreply.github.com>
2023-07-09 07:40:29 -0700

    Enable fast restart of Postgraphile by handling termination signals

--- graphql/src/server.js
+++ graphql/src/server.js
@@ -25,3 +25,30 @@ app.use(
 );
 
 http.createServer(app).listen(process.env.GRAPHQL_PORT);
+
+
+/* START: Graceful shutdown */
+// The signals we want to handle
+var signals = {
+  'SIGHUP': 1,
+  'SIGINT': 2,
+  'SIGTERM': 15
+};
+
+// Do any necessary shutdown logic for our application here
+const shutdown = (signal, value) => {
+  console.log("shutdown!");
+  server.close(() => {
+    console.log(`server stopped by ${signal} with value ${value}`);
+    process.exit(128 + value);
+  });
+};
+
+// Create a listener for each of the signals that we want to handle
+Object.keys(signals).forEach((signal) => {
+  process.on(signal, () => {
+    console.log(`process received a ${signal} signal`);
+    shutdown(signal, signals[signal]);
+  });
+});
+/* END: Graceful shutdown */
