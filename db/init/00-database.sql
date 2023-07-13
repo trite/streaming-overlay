d1073ec3d92bc3a6f3416ece75442022939f7cd6
Paul Brunner <trite@users.noreply.github.com>
2023-07-13 11:31:08 -0700

    change db to use markdown chunks

--- db/init/00-database.sql
+++ db/init/00-database.sql
@@ -10,116 +10,136 @@ BEGIN
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE TYPE public.task_status AS ENUM (
-  'TODO',
-  'BLOCKED', -- Intent: when blocked by a subtask or prereq
-  'IN_PROGRESS',
-  'DONE'
-);
-
-CREATE TABLE IF NOT EXISTS public.task (
-	id serial PRIMARY KEY,
-  to_do text NOT NULL,
-  status public.task_status NOT NULL DEFAULT 'TODO',
+CREATE TABLE IF NOT EXISTS public.markdown_chunk(
+  id SERIAL PRIMARY KEY,
+  chunk_name TEXT NOT NULL UNIQUE,
+  markdown TEXT NOT NULL,
   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
 );
 
-CREATE TRIGGER task_trigger_updated_at
-   BEFORE UPDATE ON public.task
+CREATE TRIGGER markdown_chunk_trigger_updated_at
+   BEFORE UPDATE ON public.markdown_chunk
    FOR EACH ROW
    EXECUTE PROCEDURE trigger_updated_at();
 
-CREATE TABLE IF NOT EXISTS public.relation_subtask (
-   id SERIAL PRIMARY KEY,
-   task_id INT NOT NULL REFERENCES public.task(id),
-   subtask_task_id INT NOT NULL REFERENCES public.task(id),
-   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
 
-CREATE TRIGGER relation_subtask_trigger_updated_at
-   BEFORE UPDATE ON public.relation_subtask
-   FOR EACH ROW
-   EXECUTE PROCEDURE trigger_updated_at();
 
-COMMENT ON CONSTRAINT relation_subtask_subtask_task_id_fkey
-  ON public.relation_subtask IS
-  E'@foreignFieldName subtaskOf';
 
-COMMENT ON CONSTRAINT relation_subtask_task_id_fkey
-  ON public.relation_subtask IS
-  E'@foreignFieldName subtasks';
 
-CREATE TABLE IF NOT EXISTS public.relation_prereq (
-   id SERIAL PRIMARY KEY,
-   task_id INT NOT NULL REFERENCES public.task(id),
-   prereq_task_id INT NOT NULL REFERENCES public.task(id),
-   created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-   updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
 
-CREATE TRIGGER relation_prereq_trigger_updated_at
-   BEFORE UPDATE ON public.relation_prereq
-   FOR EACH ROW
-   EXECUTE PROCEDURE trigger_updated_at();
 
-COMMENT ON CONSTRAINT relation_prereq_prereq_task_id_fkey
-  ON public.relation_prereq IS
-  E'@foreignFieldName prereqOf';
 
-COMMENT ON CONSTRAINT relation_prereq_task_id_fkey
-  ON public.relation_prereq IS
-  E'@foreignFieldName prereqs';
+-- CREATE TYPE public.task_status AS ENUM (
+--   'TODO',
+--   'BLOCKED', -- Intent: when blocked by a subtask or prereq
+--   'IN_PROGRESS',
+--   'DONE'
+-- );
 
-CREATE FUNCTION public.task_has_subtasks(task public.task)
-RETURNS TEXT AS $$
-  SELECT
-    CASE
-      WHEN EXISTS (
-        SELECT 1
-        FROM public.relation_subtask
-        WHERE task_id = task.id
-      ) THEN 'true'
-      ELSE 'false'
-    END
-$$ LANGUAGE SQL STABLE;
+-- CREATE TABLE IF NOT EXISTS public.task (
+-- 	id serial PRIMARY KEY,
+--   to_do text NOT NULL,
+--   status public.task_status NOT NULL DEFAULT 'TODO',
+--   created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
+--   updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW()
+-- );
 
-CREATE FUNCTION public.task_is_subtask(task public.task)
-RETURNS TEXT AS $$
-  SELECT
-    CASE
-      WHEN EXISTS (
-        SELECT 1
-        FROM public.relation_subtask
-        WHERE subtask_task_id = task.id
-      ) THEN 'true'
-      ELSE 'false'
-    END
-$$ LANGUAGE SQL STABLE;
+-- CREATE TRIGGER task_trigger_updated_at
+--    BEFORE UPDATE ON public.task
+--    FOR EACH ROW
+--    EXECUTE PROCEDURE trigger_updated_at();
 
-CREATE FUNCTION public.task_has_prereqs(task public.task)
-RETURNS TEXT AS $$
-  SELECT
-    CASE
-      WHEN EXISTS (
-        SELECT 1
-        FROM public.relation_prereq
-        WHERE task_id = task.id
-      ) THEN 'true'
-      ELSE 'false'
-    END
-$$ LANGUAGE SQL STABLE;
+-- CREATE TABLE IF NOT EXISTS public.relation_subtask (
+--    id SERIAL PRIMARY KEY,
+--    task_id INT NOT NULL REFERENCES public.task(id),
+--    subtask_task_id INT NOT NULL REFERENCES public.task(id),
+--    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+-- );
 
-CREATE FUNCTION public.task_is_prereq(task public.task)
-RETURNS TEXT AS $$
-  SELECT
-    CASE
-      WHEN EXISTS (
-        SELECT 1
-        FROM public.relation_prereq
-        WHERE prereq_task_id = task.id
-      ) THEN 'true'
-      ELSE 'false'
-    END
-$$ LANGUAGE SQL STABLE;
+-- CREATE TRIGGER relation_subtask_trigger_updated_at
+--    BEFORE UPDATE ON public.relation_subtask
+--    FOR EACH ROW
+--    EXECUTE PROCEDURE trigger_updated_at();
+
+-- COMMENT ON CONSTRAINT relation_subtask_subtask_task_id_fkey
+--   ON public.relation_subtask IS
+--   E'@foreignFieldName subtaskOf';
+
+-- COMMENT ON CONSTRAINT relation_subtask_task_id_fkey
+--   ON public.relation_subtask IS
+--   E'@foreignFieldName subtasks';
+
+-- CREATE TABLE IF NOT EXISTS public.relation_prereq (
+--    id SERIAL PRIMARY KEY,
+--    task_id INT NOT NULL REFERENCES public.task(id),
+--    prereq_task_id INT NOT NULL REFERENCES public.task(id),
+--    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+--    updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
+-- );
+
+-- CREATE TRIGGER relation_prereq_trigger_updated_at
+--    BEFORE UPDATE ON public.relation_prereq
+--    FOR EACH ROW
+--    EXECUTE PROCEDURE trigger_updated_at();
+
+-- COMMENT ON CONSTRAINT relation_prereq_prereq_task_id_fkey
+--   ON public.relation_prereq IS
+--   E'@foreignFieldName prereqOf';
+
+-- COMMENT ON CONSTRAINT relation_prereq_task_id_fkey
+--   ON public.relation_prereq IS
+--   E'@foreignFieldName prereqs';
+
+-- CREATE FUNCTION public.task_has_subtasks(task public.task)
+-- RETURNS TEXT AS $$
+--   SELECT
+--     CASE
+--       WHEN EXISTS (
+--         SELECT 1
+--         FROM public.relation_subtask
+--         WHERE task_id = task.id
+--       ) THEN 'true'
+--       ELSE 'false'
+--     END
+-- $$ LANGUAGE SQL STABLE;
+
+-- CREATE FUNCTION public.task_is_subtask(task public.task)
+-- RETURNS TEXT AS $$
+--   SELECT
+--     CASE
+--       WHEN EXISTS (
+--         SELECT 1
+--         FROM public.relation_subtask
+--         WHERE subtask_task_id = task.id
+--       ) THEN 'true'
+--       ELSE 'false'
+--     END
+-- $$ LANGUAGE SQL STABLE;
+
+-- CREATE FUNCTION public.task_has_prereqs(task public.task)
+-- RETURNS TEXT AS $$
+--   SELECT
+--     CASE
+--       WHEN EXISTS (
+--         SELECT 1
+--         FROM public.relation_prereq
+--         WHERE task_id = task.id
+--       ) THEN 'true'
+--       ELSE 'false'
+--     END
+-- $$ LANGUAGE SQL STABLE;
+
+-- CREATE FUNCTION public.task_is_prereq(task public.task)
+-- RETURNS TEXT AS $$
+--   SELECT
+--     CASE
+--       WHEN EXISTS (
+--         SELECT 1
+--         FROM public.relation_prereq
+--         WHERE prereq_task_id = task.id
+--       ) THEN 'true'
+--       ELSE 'false'
+--     END
+-- $$ LANGUAGE SQL STABLE;
--- db/init/01-data.sql
+++ db/init/01-data.sql
@@ -1,20 +1,38 @@
 \connect streaming-overlay;
 
-INSERT INTO public.task(to_do)
+INSERT INTO public.markdown_chunk(chunk_name, markdown)
 VALUES
-  ('Convert app to TypeScript/Postgres/GraphQL'),
-  ('Create `Control` and `Display` pages'),
-  ('Create `Control` page'),
-  ('Create `Display` page'),
-  ('Wire up subscriptions to `Display` page');
+  ('stream-overlay', '# Stream Overlay
 
-INSERT INTO public.relation_subtask(task_id, subtask_task_id)
-VALUES
-  (2, 3),
-  (2, 4);
+  ## What is this?
 
-INSERT INTO public.relation_prereq(task_id, prereq_task_id)
-VALUES
-  (2, 1),
-  (5, 4);
+  This is a web app that allows you to display markdown on your stream.'),
+  ('organization-app', '# Organization App
+
+  ## What is this?
+
+  The world clearly needs more todo/calendar/notes apps, so here''s another!');
+
+
+
+
+
+
+-- INSERT INTO public.task(to_do)
+-- VALUES
+--   ('Convert app to TypeScript/Postgres/GraphQL'),
+--   ('Create `Control` and `Display` pages'),
+--   ('Create `Control` page'),
+--   ('Create `Display` page'),
+--   ('Wire up subscriptions to `Display` page');
+
+-- INSERT INTO public.relation_subtask(task_id, subtask_task_id)
+-- VALUES
+--   (2, 3),
+--   (2, 4);
+
+-- INSERT INTO public.relation_prereq(task_id, prereq_task_id)
+-- VALUES
+--   (2, 1),
+--   (5, 4);
 
